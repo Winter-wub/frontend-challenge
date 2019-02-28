71c8f9065a51271fb5cfbe7b8100bd039fd57ec4
StratosBlur <wave_onlyone@hotmail.com>
2019-03-01 01:32:14 +0700

    Fix Deployment ✈

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
 const env = process.env.NODE_ENV;
-window.Omise.setPublicKey('pkey_test_5f1ww1annj0f10en08t');
 const api = {
 	URL:
 		env === 'development'
 			? 'http://localhost:5000'
 			: 'https://api-shop.bebebee.be',
 };
+
+window.Omise.setPublicKey('pkey_test_5f1ww1annj0f10en08t');
 
 export { api };
