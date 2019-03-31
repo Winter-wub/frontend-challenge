7ba35db5db701ddd18aee50fbab717e85dab8221
StratosBlur <wave_onlyone@hotmail.com>
2019-04-01 01:50:30 +0700

    🤼 Fix Bug login

--- src/containers/NavigationBar.js
+++ src/containers/NavigationBar.js
@@ -20,16 +20,6 @@ const signOutAction = () => dispatch => {
 		});
 };
 
-const signInWithCurrentData = () => dispatch => {
-	const userInfo = firebaseLib.auth().currentUser;
-	if (userInfo) {
-		return dispatch({
-			...userInfo,
-		});
-	} else {
-		return dispatch();
-	}
-};
 const mapDispatch = dispatch => ({
 	removeCart: index =>
 		dispatch({ type: 'REMOVE_ITEM_CART', params: { index } }),
