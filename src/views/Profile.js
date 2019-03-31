f7f4745c77ec28863b5ea40e4a5e2bc33d244d06
StratosBlur <wave_onlyone@hotmail.com>
2019-04-01 01:50:30 +0700

    🤼 Fix Bug login

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -103,16 +103,9 @@ const Profile = ({ user, saveUserData, history }) => {
 		},
 	};
 
-<<<<<<< master
-
 	useEffect(() => {
 		const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
 			if (user && !user.isLogin) {
-=======
-	useEffect(() => {
-		const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
-			if (user) {
->>>>>>> 👤 Make Auth with firebase auth
 				saveUserData(user);
 			}
 		});
