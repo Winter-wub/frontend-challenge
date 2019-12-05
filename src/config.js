0e3e20028685a6f29d678afd2b01d09ab50bd8a3
wwwwvv <wave_onlyone@hotmail.com>
2019-12-05 21:04:42 +0700

    🐳 Fix docker

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
 const env = process.env.NODE_ENV;
 const api = {
-	URL:
-		env === 'development'
-			? 'http://localhost:5000'
-			: 'https://api-shop.bebebee.be',
+	URL: env === 'development' ? 'http://localhost:5000' : 'http://13.56.249.230:2020',
 };
 
 window.Omise.setPublicKey('pkey_test_5f1ww1annj0f10en08t');
